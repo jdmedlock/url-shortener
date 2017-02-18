23d09b9a5040e7cc9cfd48f2095ee006337af70e
jdmedlock <jdmedlock@gmail.com>
2017-02-18 16:25:06 -0600

    removed mongodb:// string from dbname in config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.db = {};
 
 //config.db.host = "localhost:27017";
 //config.db.name = "urlshortener";
-config.db.host = "mongodb://heroku_63zdbcvf:qnkn8qhnpgojvqh9a17gbhh1n4@ds017246.mlab.com:17246";
+config.db.host = "heroku_63zdbcvf:qnkn8qhnpgojvqh9a17gbhh1n4@ds017246.mlab.com:17246";
 config.db.name = "heroku_63zdbcvf";
 
 module.exports = config;
