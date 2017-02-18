ff66e92255e18fc6893d15b4d821f54a64067faa
jdmedlock <jdmedlock@gmail.com>
2017-02-18 16:02:03 -0600

    added Heroku mLab host name to config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 var config = {};
 
-config.db = {}; 
+config.db = {};
 
-config.db.host = "localhost:27017";
+//config.db.host = "localhost:27017";
+config.db.host = "mongodb://heroku_63zdbcvf:qnkn8qhnpgojvqh9a17gbhh1n4@ds017246.mlab.com:17246/heroku_63zdbcvf";
 config.db.name = "urlshortener";
 
 module.exports = config;
